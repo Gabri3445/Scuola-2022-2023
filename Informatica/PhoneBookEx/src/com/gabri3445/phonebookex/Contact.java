134794c3aaed19fa4dc8d88dbd1bd32adef7ee71
Gabriele Lopez <gabriele.ope@gmail.com>
2023-04-03 17:58:03 +0200

    Contact record

---
+++ Informatica/PhoneBookEx/src/com/gabri3445/phonebookex/Contact.java
@@ -0,0 +1,11 @@
+package com.gabri3445.phonebookex;
+
+public record Contact(String name, String surname, String phoneNumber) {
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\n" +
+                "Surname: " + surname + "\n" +
+                "Phone number: " + phoneNumber;
+    }
+}
