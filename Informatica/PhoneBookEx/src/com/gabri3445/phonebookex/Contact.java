693c1a65c463a3bb3d3b3d2a4bf20370cabad15a
Gabriele Lopez <gabriele.ope@gmail.com>
2023-04-07 12:36:54 +0200

    Added annotations

--- Informatica/PhoneBookEx/src/com/gabri3445/phonebookex/Contact.java
+++ Informatica/PhoneBookEx/src/com/gabri3445/phonebookex/Contact.java
@@ -1,9 +1,13 @@
 package com.gabri3445.phonebookex;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 public record Contact(String name, String surname, String phoneNumber) {
 
+    @Contract(pure = true)
     @Override
-    public String toString() {
+    public @NotNull String toString() {
         return "Name: " + name + "\n" +
                 "Surname: " + surname + "\n" +
                 "Phone number: " + phoneNumber + "\n";
