ba9c7d1c1f262996619fced31567fcff7d4c3c00
Gabriele Lopez <gabriele.ope@gmail.com>
2023-04-17 13:16:19 +0200

    Question class

--- Informatica/Ex2/src/com/gabri3445/ex2/Question.java
+++ Informatica/Ex2/src/com/gabri3445/ex2/Question.java
@@ -1,11 +1,22 @@
 package com.gabri3445.ex2;
 
 public class Question {
-    private String question;
-    private String answer;
-    private int points;
+    private final String question;
+    private final String answer;
+    private final int points;
+
+    public Question(String question, String answer, int points) {
+        this.question = question;
+        this.answer = answer;
+        this.points = points;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
 
     public int ask(String answer) {
+
         if (this.answer.equals(answer)) {
             return points;
         }
